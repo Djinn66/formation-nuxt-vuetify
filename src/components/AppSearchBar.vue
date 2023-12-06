7bf0917890e8f6f51d4c744c96ab57abc96680a6
Beliol <paul.mauconduitpro@hotmail.com>
2023-12-06 09:53:37 +0100

    feat(search): Ajout du composant AppSearchBar

---
+++ src/components/AppSearchBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  searchItem: string;
+}
+
+const props = defineProps<Props>();
+
+const emits = defineEmits<{ "update:search": [value: string] }>();
+</script>
+
+<template>
+  <v-text-field
+    v-bind="searchItem"
+    @input="$emit('update:search', $event)"
+    label="Search"
+    hide-details
+    prepend-icon="mdi-magnify"
+  ></v-text-field>
+</template>
+
+<style scoped></style>
